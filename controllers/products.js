98a9546cbc463253debcb8476fe59b879046d3b4
abijith <abhijithwilson123@gmail.com>
2023-08-19 08:44:45 +0530

    Initial commit

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,21 @@
+const product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "Products Testing Route" });
+  const products = await product.find({
+    featured: true,
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Products Route" });
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  const products = await product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
